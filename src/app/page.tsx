c39d9e2dc5b8c8f92f0e2c3c399caa9fceabe0d2
BrandonSRA <brandon33455@gmail.com>
2023-11-14 19:39:47 -0500

    fix: :bug: fix the server render in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,14 @@ interface Singer {
 export default async function Home() {
   const singers = await fetch(
     `${process.env.NEXT_PUBLIC_API_URL}/api/singers`
-  ).then((res) => res.json() as Promise<Singer[]>);
+  ).then((res) => {
+    if (res.ok) {
+      return res.json() as Promise<Singer[]>;
+    } else {
+      return [];
+    }
+  });
+
   console.log(singers);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
